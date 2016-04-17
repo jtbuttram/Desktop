1cb516edcad527b7e58247667d54e43fe6ad2e56
John Buttram <jtbuttram@gmail.com>
2016-04-17 10:50:15 -0400

    done working on Zombits for a while

--- Py/performance.py
+++ Py/performance.py
@@ -3,15 +3,17 @@ import solution_zombits
 import sandbox
 from random import randint
 
+
 def buildPopulation(x, y, sMax):
-    population = [[randint(0,sMax) for j in range(y)] for i in range(x)]
+    population = [[randint(0, sMax) for j in range(y)] for i in range(x)]
     return population
+
 
 def printArr(array):
     for i in range(len(array)):
         print(array[i])
 
-n = 10000
+n = 1000
 size = 50
 sMax = 9
 
@@ -19,28 +21,28 @@ p = []
 p1 = []
 
 for i in xrange(n):
-	a = buildPopulation(size, size, sMax)
-	b = randint(0, size-1)
-	c = randint(0, size-1)
-	d = randint(0, sMax)
-	p.append([a, b, c, d])
-	p1.append([a, b, c, d])
+    a = buildPopulation(size, size, sMax)
+    b = randint(0, size-1)
+    c = randint(0, size-1)
+    d = randint(0, sMax)
+    p.append([a, b, c, d])
+    p1.append([a, b, c, d])
 
 print('Ready... Go!')
 
 start = timer()
 while p:
     i = p.pop()
-    a = 1 # solution_zombits.answer(i[0],i[1],i[2],i[3])
+    a = solution_zombits.answer(i[0], i[1], i[2], i[3])
 end = timer()
-#print(start, end)
+# print(start, end)
 print(end-start)
 
 start = timer()
 while p1:
-    i = p1.pop()
-    a = 1 # sandbox.answer(i[0],i[1],i[2],i[3])
+    j = p1.pop()
+    b = sandbox.answer(j[0], j[1], j[2], j[3])
 end = timer()
-#print(start, end)
+# print(start, end)
 print(end-start)
-#printArr(b)
+# printArr(b)
